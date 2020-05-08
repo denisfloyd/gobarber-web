be65d973522814fef4a83fd66dccac31bd210792
denismend <denisladeira1@gmail.com>
2020-05-07 21:59:00 -0300

    Sign in Signup pages

--- src/pages/SignUp/styles.ts
+++ src/pages/SignUp/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import { shade } from 'polished';
 
-import SignInBackgroundImg from '../../assets/sign-in-background.png';
+import SignUpBackgroundImg from '../../assets/sign-up-background.png';
 
 export const Container = styled.div`
   height: 100vh;
@@ -43,14 +43,14 @@ export const Content = styled.div`
   }
 
   > a {
-    color: #ff9000;
+    color: #f3ede8;
     display: block;
     margin-top: 24px;
     text-decoration: none;
     transition: color 0.2s;
 
     &:hover {
-      color: ${shade(0.2, '#ff9000')};
+      color: ${shade(0.2, '#f3ede8')};
     }
 
     display: flex;
@@ -64,6 +64,6 @@ export const Content = styled.div`
 
 export const Background = styled.div`
   flex: 1;
-  background: url(${SignInBackgroundImg}) no-repeat center;
+  background: url(${SignUpBackgroundImg}) no-repeat center;
   background-size: cover;
 `;
