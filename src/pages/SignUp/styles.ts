39e3df7e9256fd0b0b8ce9096ec3ac4f4a0ed319
denismend <denisladeira1@gmail.com>
2020-05-10 15:51:15 -0300

    routes

--- src/pages/SignUp/styles.ts
+++ src/pages/SignUp/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { shade } from 'polished';
 
 import SignUpBackgroundImg from '../../assets/sign-up-background.png';
@@ -14,11 +14,30 @@ export const Content = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-
-  place-content: center;
+  justify-content: center;
 
   width: 100%;
   max-width: 650px;
+`;
+
+const appearFromRight = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(50px);
+  },
+  to {
+    opacity: 1;
+    transform: translateX(0px);
+  }
+`;
+
+export const AnimationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  animation: ${appearFromRight} 1s;
 
   form {
     margin: 60px 0;
@@ -43,14 +62,14 @@ export const Content = styled.div`
   }
 
   > a {
-    color: #f3ede8;
+    color: #ff9000;
     display: block;
     margin-top: 24px;
     text-decoration: none;
     transition: color 0.2s;
 
     &:hover {
-      color: ${shade(0.2, '#f3ede8')};
+      color: ${shade(0.2, '#ff9000')};
     }
 
     display: flex;
