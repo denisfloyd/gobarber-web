8e06feac3039695fd7b851146b8ca77de517d634
denismend <denisladeira1@gmail.com>
2020-07-13 21:42:02 -0300

    tests

--- src/__Tests__/pages/SignIn.spec.tsx
+++ src/__Tests__/pages/SignIn.spec.tsx
@@ -1,18 +1,35 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import SignIn from '../../pages/SignIn';
+
+const mockHistoryPush = jest.fn();
 
 jest.mock('react-router-dom', () => {
   return {
-    useHistory: jest.fn(),
+    useHistory: () => ({
+      push: mockHistoryPush,
+    }),
     Link: ({ children }: { children: React.ReactNode }) => children,
   };
 });
 
 describe('SignIn Page', () => {
   it('should be able to sign in', () => {
-    const { debug } = render(<SignIn />);
+    const { getByPlaceholderText, getByText } = render(<SignIn />);
 
-    debug();
+    const emailField = getByPlaceholderText('E-mail');
+    const passwordField = getByPlaceholderText('password');
+    const buttonElement = getByText('Entrar');
+
+    fireEvent.change(emailField, {
+      target: { value: 'denisladeira1@gmail.com' },
+    });
+    fireEvent.change(passwordField, {
+      target: { value: '123456' },
+    });
+
+    fireEvent.click(buttonElement);
+
+    expect(mockHistoryPush).toHaveBeenCalledWith('/dashboard');
   });
 });
