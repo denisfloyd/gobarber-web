022cd863d34361b42a414c014cdadf10ea6aefa4
denismend <denisladeira1@gmail.com>
2020-07-14 21:51:21 -0300

    tests

--- src/__Tests__/pages/SignIn.spec.tsx
+++ src/__Tests__/pages/SignIn.spec.tsx
@@ -42,4 +42,25 @@ describe('SignIn Page', () => {
       expect(mockHistoryPush).toHaveBeenCalledWith('/dashboard');
     });
   });
+
+  it('should not be able to sign in with invalid credentials', async () => {
+    const { getByPlaceholderText, getByText } = render(<SignIn />);
+
+    const emailField = getByPlaceholderText('E-mail');
+    const passwordField = getByPlaceholderText('password');
+    const buttonElement = getByText('Entrar');
+
+    fireEvent.change(emailField, {
+      target: { value: 'not-valid-email' },
+    });
+    fireEvent.change(passwordField, {
+      target: { value: '123456' },
+    });
+
+    fireEvent.click(buttonElement);
+
+    await wait(() => {
+      expect(mockHistoryPush).toHaveBeenCalledWith('/dashboard');
+    });
+  });
 });
