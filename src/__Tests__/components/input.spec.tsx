da35f98804c34f9702ac28a9385fcceba3314e24
Denis M. Ladeira <denisladeira1@gmail.com>
2020-08-02 22:14:00 -0300

    input tests

--- src/__Tests__/components/input.spec.tsx
+++ src/__Tests__/components/input.spec.tsx
@@ -39,5 +39,33 @@ describe('Input Component', () => {
       expect(containerElement).toHaveStyle('border-color: #ff9000;');
       expect(containerElement).toHaveStyle('color: #ff9000;');
     });
+
+    fireEvent.blur(inputElement);
+
+    await wait(() => {
+      expect(containerElement).not.toHaveStyle('border-color: #ff9000;');
+      expect(containerElement).not.toHaveStyle('color: #ff9000;');
+    });
+  });
+
+  it('should keep input border highlight when input filled', async () => {
+    const { getByPlaceholderText, getByTestId } = render(
+      <Input name="email" placeholder="E-mail" />,
+    );
+
+    const inputElement = getByPlaceholderText('E-mail');
+    const containerElement = getByTestId('input-container');
+
+    fireEvent.change(inputElement, {
+      target: {
+        value: 'johndoe@example.com.br',
+      },
+    });
+
+    fireEvent.blur(inputElement);
+
+    await wait(() => {
+      expect(containerElement).toHaveStyle('color: #ff9000;');
+    });
   });
 });
